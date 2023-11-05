6994ef4ba00bd5156f91e582c38cbd778fc21d6d
Mateus Silva <mateussilva@ntopus.com.br>
2023-11-04 21:59:55 -0300

    teste cd terraform 64

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -1,7 +1,7 @@
 import {log} from 'log';
 
 export const handler = async (event) => {
-    log('log de execução' + JSON.stringify(event))
+    log('log de execução apos commit' + JSON.stringify(event))
     return {
         statusCode: 200,
         body: JSON.stringify(event),
